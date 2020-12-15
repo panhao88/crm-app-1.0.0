31315f5f910ad5841dee5beb2a7451edacdbb4a1
潘浩 <1124459050@qq.com>
2020-12-15 14:13:50 +0800

    14.13

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,37 @@
 <template>
-  <div>
-    <div>
-      <input type="text" name="" v-model="value" id="" @keydown.enter="didi" />
-    </div>
-  </div>
+ <div>
+  11111
+ </div>
 </template>
 
 <script>
-export default {
-  name: "",
-  props: {},
-  components: {},
-  data() {
-    return {
-      value: "",
-    };
-  },
-  methods: {
-    didi() {
-      let arr = this.value;
-      if( isNaN(arr)){
-          alert("强填写数字")
-      }
-    },
-  },
-  mounted() {
-      
-  },
-  watch: {},
-  computed: {},
-  filters: {},
-};
+ export default {
+   name: '',
+   props: {
+   },
+   components: {
+
+   },
+   data () {
+     return {
+
+     }
+   },
+   methods: {
+
+   },
+   mounted() {
+
+   },
+   watch: {
+
+   },
+   computed: {
+
+   }
+ }
 </script>
 
 <style scoped lang='scss'>
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
           // target: 'http://211.149.157.5:85',
           // target: 'http://ce.96291.club:82',
           //  target: 'http://211.149.157.5:83',
-          target: 'http://192.168.0.25',
+          target: 'http://192.168.0.25:8080',
           changeOrigin: true,
           pathRewrite: {
             '^/api': ''
